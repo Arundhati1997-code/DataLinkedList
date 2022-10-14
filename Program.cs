c421d7e0a05c97d5b5383b386c0d6aa6c34cfd52
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-14 11:19:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DataLinkedList;
+
+LinkedLists linkedLists = new LinkedLists();
+
+linkedLists.AppendNode(56);
+linkedLists.AppendNode(30);
+linkedLists.AppendNode(40);
+linkedLists.AppendNode(70);
+linkedLists.sort();
+linkedLists.Display();
